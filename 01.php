80c907f7a9e55cd510f74480d0decb426f90dd5f
n-ibrahimi <nasim.hamdard21@gmail.com>
2019-05-11 02:28:32 +0200

    2

--- 01.php
+++ 01.php
@@ -1,44 +1,59 @@
 
 <?php 
 
+// yes or no cheaking variable 
+$action=null; 
 $workingtime=[];
 
-$workingtime[0]['open']=(int)readline('enter the opening time: ');
-$workingtime[0]['close']=(int)readline('enter the closing time: ');
+//boucle while  
+while($action !== 'n'){
 
-        if($workingtime[0]['open'] > $workingtime[0]['close']){
-            echo "the entered time is invalid \ntry again !";
+        if ($action !== 'n'){
+// setting the time 
+            $open=(int)readline('enter the opening time: ');
+            $close=(int)readline('enter the closing time: ');
+
+        // cheacking the end time is biger the starting time
+                if($open > $close){
+                    echo "the entered time is invalid \ntry again !";
+                }
+
+            //saving the time 
+            $workingtime[]=[$open, $close];
+
         }
-$i=0;
-$action=null; 
 
-while($action != 'no'){
-
-    $action=readline("Do you want to enter another time? ");
-
-if ($action != 'no'){
-
-    $i+=1;
-    $workingtime[$i]['open']=$open=(int)readline('enter the opening time: ');
-    $workingtime[$i]['close']=$close=(int)readline('enter the closing time: ');
-
-            if($workingtime[$i]['open'] > $workingtime[$i]['close']){
-                echo "the entered time is invalid \n try again !";
-            }
-        } else {
-
-            echo "Merci\n";
-            echo "******************************************\n";
-        }
+        $action=readline("Do you want to enter another times? (o/n) => ");
 
     }
 
- $x=0;
-    foreach($workingtime as $work){
-      
-        echo '- our shop is open between '. $work['open']. ' and '. $work['close']. " everyday\n";
-    };
+    echo "******************************************\n";
+    echo "Merci\n";
+    echo "******************************************\n\n\n";
 
+
+    //asking at what time he wants to go to shop?
+
+    $going=(int)readline("what time do you want to go at shop? =>");
+    $is_open=false;
+    foreach($workingtime as $hour){
+        if($going >= $hour[0] && $going <= $hour[1]){
+            echo "\n\nthe shop is open !\n\n";
+            $is_open=true; 
+        }
+    }
+    
+    if($is_open !=true ){
+        echo "\n\nthe shop is close !\n\n";
+    }
+
+
+
+    echo "******************************************\n";
+    foreach($workingtime as $work){
+        echo '- our shop is open between '. $work[0]. ' and '. $work[1]. "\n";
+    };
+    echo "******************************************\n";
 
 ?>
 
